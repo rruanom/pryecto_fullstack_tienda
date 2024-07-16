f8360658414e08565dc0290aabf3eb046cbd9c11
(Robi) <(r.ruano3@gmail.com)>
2024-07-17 00:21:19 +0200

    filtros en la parte de home

--- queries/products.queries.js
+++ queries/products.queries.js
@@ -1,25 +1,19 @@
 const queries = {
-    getProductsByFilters:`
-    SELECT *
-    FROM (
-    SELECT *, c.name AS category_name
+    getProductsByFilters: `
+    SELECT p.name, p.image, p.price, p.id_category, c.name AS category, prov.name AS provider
     FROM products p
     JOIN categories c ON p.id_category = c.id_category
-    WHERE LOWER(c.name) LIKE LOWER('%' || $1 || '%')
-    ) AS byCategory
-    JOIN (
-    SELECT *, prov.name AS provider_name
-    FROM products p
     JOIN providers prov ON p.id_provider = prov.id_provider
-    WHERE LOWER(prov.name) LIKE LOWER('%' || $2 || '%')
-    ) AS byProvider ON byCategory.id_product = byProvider.id_product
-    WHERE LOWER(byProvider.provider_name) LIKE LOWER('%' || $3 || '%')
+    WHERE 1=1
+      AND ($1 = '' OR LOWER(c.name) LIKE LOWER('%' || $1 || '%'))
+      AND ($2 = '' OR LOWER(prov.name) LIKE LOWER('%' || $2 || '%'))
+      AND ($3 = '' OR LOWER(p.name) LIKE LOWER('%' || $3 || '%'))
     ORDER BY RANDOM()
-    LIMIT 10 OFFSET $1;`,
+    LIMIT $4 OFFSET $5;`,
     //para hacer la paginacion de 10 en 10 en Home de manera aleatoria
     getTenProductsRandom: `
     SELECT p.name, p.image, p.price, p.id_category,
-       prov.name AS provider, c.name AS category
+    prov.name AS provider, c.name AS category
     FROM products p
     JOIN providers prov ON p.id_provider = prov.id_provider
     JOIN categories c ON p.id_category = c.id_category
@@ -27,7 +21,7 @@ const queries = {
     LIMIT 10 OFFSET $1;
     `,
     updateProducts:
-    `SET
+        `SET
     description = COALESCE($1, description),
     price = COALESCE($2, price),
     image = COALESCE($3, image),
@@ -38,7 +32,7 @@ const queries = {
     DELETE FROM products
     WHERE name = $1
     RETURNING *;`,
-    createProduct:`
+    createProduct: `
     INSERT INTO products (name, description, price, image, id_provider, id_category)
     SELECT $1, $2, $3, $4, p.id_provider, c.id_category
     FROM providers p
