e7c10ed2c57583c73d23d97e412b08e687ab256e
(Robi) <(r.ruano3@gmail.com)>
2024-07-22 00:50:50 +0200

    ajustes ruta render

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Ruta catch-all para SPA
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
 
 // Middleware para manejo de 404
