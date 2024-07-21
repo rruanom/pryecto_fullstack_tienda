477aa4857a2ba5adc2b1a9f02f2b29add3e92f01
(Robi) <(r.ruano3@gmail.com)>
2024-07-22 00:35:25 +0200

    puerto

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const swaggerDocument = require('./swagger.json');
 require('./config/db_mongo');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 // Configuración de CORS
 const corsOptions = {
